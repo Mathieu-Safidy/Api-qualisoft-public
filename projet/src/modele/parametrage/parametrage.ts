714fa48cb22929c5054c544dbf56d0ad4d9010b7
Mathieu RAKOTOARISOA <148275593+Mathieu-Safidy@users.noreply.github.com>
2025-08-06 15:15:49 +0300

    Modele parametrage

---
+++ projet/src/modele/parametrage/parametrage.ts
@@ -0,0 +1,23 @@
+import { TypeErreur } from "../erreur/Type_Erreur";
+import { ObjectifQualite } from "../objectIf/ObjectifQualite";
+
+export class Parametrage {
+    client_nom: string = 'defaultClient';
+    interlocuteur_nom: string = '';
+    contact_interlocuteur: string = '';
+    cp_responsable: string = '';
+    description_traite: string = '';
+    type_traite: string = '';
+    ligne?: string;
+    plan?: string;
+    fonction?: string;
+    objectif_qualite: ObjectifQualite[] = [];
+    type_erreur: any[] = [];
+    colonne: any[] = [];
+
+    constructor(init?: Partial<Parametrage>) {
+        Object.assign(this, init);
+    }
+
+    
+}
