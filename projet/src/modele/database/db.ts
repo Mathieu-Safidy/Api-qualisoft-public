89b7a04ff33a31f8e001460ba37bc79bb5345114
arindranto <arindranto.safidy@gmail.com>
2025-08-14 17:12:53 +0300

    Added deployment

--- projet/src/modele/database/db.ts
+++ projet/src/modele/database/db.ts
@@ -5,11 +5,12 @@ export let pool: Pool | undefined = undefined
 export function initDb() {
     console.log("Creating pool...")
 	pool = new Pool({
-		host: process.env.PG_HOST,
-		port: parseInt(process.env.PG_PORT || '5432'),
-		user: process.env.PG_USER,
-		password: process.env.PG_PASSWORD,
-		database: process.env.PG_DATABASE,
+		// host: process.env.PG_HOST,
+		// port: parseInt(process.env.PG_PORT || '5432'),
+		// user: process.env.PG_USER,
+		// password: process.env.PG_PASSWORD,
+		// database: process.env.PG_DATABASE,
+		connectionString: process.env.DATABASE_URL
 	})
 }
 
